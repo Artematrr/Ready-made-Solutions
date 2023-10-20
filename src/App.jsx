239da57bdcac7f9b91647b1980dc7485d312eb9a
Artem Bobylev <artematrr@gmail.com>
2023-10-20 17:20:36 +0400

    upd files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css'
+import './styles.css'
+import HomePage from './Templates/HomePage/HomePage'
 
 function App() {
   return (
     <>
-      Hello
+      <HomePage/ >
     </>
   )
 }
