0b4b9e921c3bf1503d81d09db94333669c00fe13
Artem Bobylev <artematrr@gmail.com>
2023-11-27 13:44:41 +0400

    upd tpl

---
+++ src/components/Chunks/Product.jsx
@@ -0,0 +1,11 @@
+import Breadcrumbs from './Breadcrumbs'
+
+const Product = () => {
+	return (
+		<>
+			<Breadcrumbs />
+		</>
+	)
+}
+
+export default Product
---
+++ src/components/Chunks/Sidebar.jsx
@@ -0,0 +1,13 @@
+const Sidebar = () => {
+  return (
+    <aside className='sidebar'>
+      <div className='logo'>
+        <img src='/src/assets/img/logo.svg' alt='Готовые Решения' />
+      </div>
+
+      <div className='menu-label'>Каталог сайтов</div>
+    </aside>
+  )
+}
+
+export default Sidebar
