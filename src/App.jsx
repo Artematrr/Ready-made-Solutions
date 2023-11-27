91f314c83d6b6670cfb30f1c62a56ac08f4b3ca4
Artem Bobylev <artematrr@gmail.com>
2023-11-27 13:46:17 +0400

    upd page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import Footer from './components/Chunks/Footer'
+// import Header from './components/Chunks/Header'
+import Sidebar from './components/Chunks/Sidebar'
 import './styles.css'
-import HomePage from './Templates/HomePage/HomePage'
+import HomePage from './Templates/HomePage'
 
 function App() {
   return (
-    <>
-      <HomePage/ >
-    </>
+    <div className='page-wrapper'>
+      <div className='content-wrapper'>
+        <Sidebar />
+        <HomePage />
+      </div>
+      <Footer />
+    </div>
   )
 }
 
