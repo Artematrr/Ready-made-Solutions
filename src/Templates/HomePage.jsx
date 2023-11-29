2fa2a1547e456c3db27e1da27b0541dad45207b8
Artem Bobylev <artematrr@gmail.com>
2023-11-29 14:52:21 +0400

    upd tpl

--- src/Templates/HomePage.jsx
+++ src/Templates/HomePage.jsx
@@ -2,18 +2,19 @@ import Header from '../components/Chunks/Header'
 
 const HomePage = () => {
   return (
-    <main className='main'>
+    <>
+      <main className='main'>
       <Header />
-
-      <div className='container'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
-        aspernatur nihil quis iste minus velit! Architecto atque delectus
-        voluptates nesciunt eos iste doloribus similique soluta, id, illum
-        officiis culpa eligendi aliquam inventore dicta dolore saepe sed tenetur
-        reiciendis voluptas, illo voluptatem. Velit nesciunt odit voluptate
-        animi sit eum illum alias.
-      </div>
-    </main>
+        <div className='container'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
+          aspernatur nihil quis iste minus velit! Architecto atque delectus
+          voluptates nesciunt eos iste doloribus similique soluta, id, illum
+          officiis culpa eligendi aliquam inventore dicta dolore saepe sed
+          tenetur reiciendis voluptas, illo voluptatem. Velit nesciunt odit
+          voluptate animi sit eum illum alias.
+        </div>
+      </main>
+    </>
   )
 }
 
---
+++ src/Templates/Product.jsx
@@ -0,0 +1,17 @@
+import Breadcrumbs from '../components/Chunks/Breadcrumbs'
+import Header from '../components/Chunks/Header'
+
+const Product = () => {
+  return (
+    <>
+      <main className='main'>
+        <Header />
+        <div className='container'>
+          <Breadcrumbs />
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default Product
