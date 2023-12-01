b870d65f6a0f94acba57727254f55e80a51702be
Artem Bobylev <artematrr@gmail.com>
2023-12-01 17:14:30 +0400

    add form and tags block for sidebar

--- src/components/Chunks/Sidebar.jsx
+++ src/components/Chunks/Sidebar.jsx
@@ -212,6 +212,56 @@ const Sidebar = () => {
             </li>
           </ul>
         </div>
+        <div className='sidebar-form-container'>
+          <div className='sidebar-form-title'>
+            Не знаете, какое готовое решение вам подойдёт?
+          </div>
+          <div className='sidebar-form-img'>
+            <img src='img/sidebar-form-image.svg' alt='sidebar-form-image' />
+          </div>
+          <div className='sidebar-form-label'>
+            Заполните форму и мы поможем Вам подобрать оптимальное решение
+          </div>
+          <form className='sidebar-form-inputs' action=''>
+            <input type='text' id='name' placeholder='Ваше имя' />
+            <row>
+            <input
+              type='tel'
+              id='phone'
+              pattern='\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}'
+              placeholder='+7 (_ _ _) _ _ _ - _ _ - _ _'
+            />
+            <select id='area'>
+              Сфера деятельности
+              <option value='0'>Сфера деятельности</option>
+              <option value='Строительство'>Строительство</option>
+              <option value='Медицина'>Медицина</option>
+            </select>
+            </row>
+            <div className='sidebar-form-label-button'>
+              Нажимая на кнопку, вы даёте согласие на обработку Персональных
+              данных
+            </div>
+            
+            <button className='sidebar-form-button button-primary'>
+              Получить предложение
+            </button>
+          </form>
+        </div>
+
+        <div className='sidebar-tags-label'>Сферы бизнеса</div>
+        <div className='sidebar-tags'>
+          <a>Лендинги</a>
+          <a>Интернет-магазины</a>
+          <a>Корпоративные</a>
+          <a>Лендинги</a>
+          <a>Интернет-магазины</a>
+          <a>Корпоративные</a>
+          <a>Лендинги</a>
+          <a>Интернет-магазины</a>
+          <a>Корпоративные</a>
+          <a>Визитки</a>
+        </div>
       </div>
     </aside>
   )
