d654118a2e8edd1fea4029cb2ee7f7687fea0ad1
Artem Bobylev <artematrr@gmail.com>
2023-12-04 20:34:27 +0400

    rename 'Chunk' to 'elements'

--- src/components/elements/Header.jsx
+++ src/components/elements/Header.jsx
@@ -4,6 +4,23 @@ const Header = () => {
       <nav className='nav-container'>
         <div className='nav-button nav-city'>
           <a href='#'>Москва</a>
+          <div className='nav-city-dropdown'>
+            <a data-city='Москва' href='#'>
+              Москва
+            </a>
+            <a data-city='Самара' href='#'>
+              Самара
+            </a>
+            <a data-city='Санкт-Петербург' href='#'>
+              Санкт-Петербург
+            </a>
+            <a data-city='Ростов-на-Дону' href='#'>
+              Ростов-на-Дону
+            </a>
+            <a data-city='Набережные Челны' href='#'>
+              Набережные Челны
+            </a>
+          </div>
         </div>
         <div className='nav-button'>
           <a href='#'>Каталог сайтов</a>
@@ -41,6 +58,7 @@ const Header = () => {
               alt='Избранное'
               title='Избранное'
             ></img>
+            <div className='header-favourites-count'>100</div>
           </div>
           <div className='header-cart'>
             <img
@@ -48,6 +66,7 @@ const Header = () => {
               alt='Корзина'
               title='Корзина'
             />
+            <div className='header-cart-count'>100</div>
           </div>
           <div className='header-profile'>
             <img
--- src/components/elements/Sidebar.jsx
+++ src/components/elements/Sidebar.jsx
@@ -14,66 +14,68 @@ const Sidebar = () => {
                   src='img/icons/item-universal.svg'
                   alt='Универсальный дизайн'
                 />
-                Универсальный дизайн
+                <span>Универсальный дизайн</span>
                 <span className='item-count'>10</span>
               </a>
             </li>
             <li className='sidebar-nav-item level-1 open'>
               <a href='#'>
                 <img src='img/icons/item-food.svg' alt='Еда, напитки' />
-                Еда, напитки
+                <span>Еда, напитки</span>
                 <span className='item-count'>100</span>
               </a>
               <ul className='sidebar-nav-list'>
                 <li className='sidebar-nav-item level-2'>
                   <a href='#'>
-                    Подуктовые сайты
+                    <span>Подуктовые сайты</span>
                     <span className='item-count'>10</span>
                   </a>
                 </li>
                 <li className='sidebar-nav-item level-2'>
                   <a href='#'>
-                    Для бара
+                    <span>Для бара</span>
                     <span className='item-count'>10</span>
                   </a>
                 </li>
                 <li className='sidebar-nav-item level-2'>
                   <a href='#'>
-                    Для кондитерской
+                    <span>Для кондитерской</span>
                     <span className='item-count'>10</span>
                   </a>
                 </li>
                 <li className='sidebar-nav-item level-2'>
                   <a href='#'>
-                    Для кофейни
+                    <span>Для кофейни</span>
                     <span className='item-count'>10</span>
                   </a>
                 </li>
                 <li className='sidebar-nav-item level-2'>
                   <a href='#'>
-                    Для суши и роллы
+                    <span>Для суши и роллы</span>
                     <span className='item-count'>10</span>
                   </a>
                 </li>
                 <li className='sidebar-nav-item level-2'>
                   <a href='#'>
-                    Для фаст фуда
+                    <span>Для фаст фуда</span>
                     <span className='item-count'>10</span>
                   </a>
                 </li>
                 <li className='sidebar-nav-item level-2'>
                   <a href='#'>
-                    Для доставки еды<span className='item-count'>10</span>
+                    <span>Для доставки еды</span>
+                    <span className='item-count'>10</span>
                   </a>
                 </li>
                 <li className='sidebar-nav-item level-2'>
                   <a href='#'>
-                    Для ресторана<span className='item-count'>10</span>
+                    <span>Для ресторана</span>
+                    <span className='item-count'>10</span>
                   </a>
                 </li>
                 <li className='sidebar-nav-item level-2'>
                   <a href='#'>
-                    Для продажи рыбной продукции
+                    <span>Для продажи рыбной продукции</span>
                     <span className='item-count'>10</span>
                   </a>
                 </li>
@@ -85,25 +87,25 @@ const Sidebar = () => {
                   src='img/icons/item-medicine.svg'
                   alt='Медицина, красота, здоровье'
                 />
-                Медицина, красота, здоровье
+                <span>Медицина, красота, здоровье</span>
                 <span className='item-count'>100</span>
               </a>
               <ul className='sidebar-nav-list'>
                 <li className='sidebar-nav-item level-2'>
                   <a href='#'>
-                    Подуктовые сайты
+                    <span>Подуктовые сайты</span>
                     <span className='item-count'>10</span>
                   </a>
                 </li>
                 <li className='sidebar-nav-item level-2'>
                   <a href='#'>
-                    Для бара
+                    <span>Для бара</span>
                     <span className='item-count'>10</span>
                   </a>
                 </li>
                 <li className='sidebar-nav-item level-2'>
                   <a href='#'>
-                    Для кондитерской
+                    <span>Для кондитерской</span>
                     <span className='item-count'>10</span>
                   </a>
                 </li>
@@ -115,14 +117,14 @@ const Sidebar = () => {
                   src='img/icons/item-build.svg'
                   alt='Строительство, ремонт'
                 />
-                Строительство, ремонт
+                <span>Строительство, ремонт</span>
                 <span className='item-count'>100</span>
               </a>
             </li>
             <li className='sidebar-nav-item level-1'>
               <a href='#'>
                 <img src='img/icons/item-services.svg' alt='Услугм' />
-                Услугм
+                <span>Услугм</span>
                 <span className='item-count'>100</span>
               </a>
             </li>
@@ -132,7 +134,7 @@ const Sidebar = () => {
                   src='img/icons/item-education.svg'
                   alt='Образование, курсы, сми'
                 />
-                Образование, курсы, сми
+                <span>Образование, курсы, сми</span>
                 <span className='item-count'>100</span>
               </a>
             </li>
@@ -142,7 +144,7 @@ const Sidebar = () => {
                   src='img/icons/item-transport.svg'
                   alt='Транспорт, спецтехника'
                 />
-                Транспорт, спецтехника
+                <span>Транспорт, спецтехника</span>
                 <span className='item-count'>100</span>
               </a>
             </li>
@@ -210,6 +212,90 @@ const Sidebar = () => {
                 </li>
               </ul>
             </li>
+            <li className='sidebar-nav-item level-1'>
+              <a href='#'>
+                <img
+                  src='img/icons/item-entertainment.svg'
+                  alt='Досуг, развлечения, искусство'
+                />
+                <span>Досуг, развлечения, искусство</span>
+                <span className='item-count'>100</span>
+              </a>
+            </li>
+            <li className='sidebar-nav-item level-1'>
+              <a href='#'>
+                <img
+                  src='img/icons/item-sport.svg'
+                  alt='Транспорт, спецтехника'
+                />
+                <span>Спорт и туризм</span>
+                <span className='item-count'>100</span>
+              </a>
+            </li>
+            <li className='sidebar-nav-item level-1'>
+              <a href='#'>
+                <img
+                  src='img/icons/item-society.svg'
+                  alt='Государство и общество'
+                />
+                <span>Государство и общество</span>
+                <span className='item-count'>100</span>
+              </a>
+            </li>
+            <li className='sidebar-nav-item level-1'>
+              <a href='#'>
+                <img src='img/icons/item-lendings.svg' alt='Лендинги' />
+                <span> Лендинги</span>
+                <span className='item-count'>100</span>
+              </a>
+            </li>
+            <li className='sidebar-nav-item level-1'>
+              <a href='#'>
+                <img src='img/icons/item-e-shop.svg' alt='Интернет-магазины' />
+                <span> Интернет-магазины</span>
+                <span className='item-count'>100</span>
+              </a>
+            </li>
+            <li className='sidebar-nav-item level-1'>
+              <a href='#'>
+                <img
+                  src='img/icons/item-business-card.svg'
+                  alt='Сайты визитки'
+                />
+                <span> Сайты визитки</span>
+                <span className='item-count'>100</span>
+              </a>
+            </li>
+            <li className='sidebar-nav-item level-1'>
+              <a href='#'>
+                <img
+                  src='img/icons/item-corporate.svg'
+                  alt='Корпоративные сайты'
+                />
+                <span> Корпоративные сайты</span>
+                <span className='item-count'>100</span>
+              </a>
+            </li>
+            <li className='sidebar-nav-item level-1'>
+              <a href='#'>
+                <img
+                  src='img/icons/item-single-page.svg'
+                  alt='Одностраничные сайты'
+                />
+                <span> Одностраничные сайты</span>
+                <span className='item-count'>100</span>
+              </a>
+            </li>
+            <li className='sidebar-nav-item level-1'>
+              <a href='#'>
+                <img
+                  src='img/icons/item-announcement.svg'
+                  alt='Сайты объявлений'
+                />
+                <span> Сайты объявлений</span>
+                <span className='item-count'>100</span>
+              </a>
+            </li>
           </ul>
         </div>
         <div className='sidebar-form-container'>
@@ -224,25 +310,25 @@ const Sidebar = () => {
           </div>
           <form className='sidebar-form-inputs' action=''>
             <input type='text' id='name' placeholder='Ваше имя' />
-            <div className="row">
-            <input
-              type='tel'
-              id='phone'
-              pattern='\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}'
-              placeholder='+7 (_ _ _) _ _ _ - _ _ - _ _'
-            />
-            <select id='area'>
-              Сфера деятельности
-              <option value='0'>Сфера деятельности</option>
-              <option value='Строительство'>Строительство</option>
-              <option value='Медицина'>Медицина</option>
-            </select>
+            <div className='row'>
+              <input
+                type='tel'
+                id='phone'
+                pattern='\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}'
+                placeholder='+7 (_ _ _) _ _ _ - _ _ - _ _'
+              />
+              <select id='area'>
+                Сфера деятельности
+                <option value='0'>Сфера деятельности</option>
+                <option value='Строительство'>Строительство</option>
+                <option value='Медицина'>Медицина</option>
+              </select>
             </div>
             <div className='sidebar-form-label-button'>
               Нажимая на кнопку, вы даёте согласие на обработку Персональных
               данных
             </div>
-            
+
             <button className='sidebar-form-button button-primary'>
               Получить предложение
             </button>
